ec7a038dc3aeb0e3da3be305329717f9af2493d7
Ragin <mmu@ragin.org>
2020-08-14 14:09:29 +0200

    Updates

---
+++ precache-manifest.82a904e5413e552f80bf7c4bf85cee01.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "504d693f4276d075c95230473d9999a2",
+    "url": "/raginreact/index.html"
+  },
+  {
+    "revision": "ec83b8b3e2f59dfc76e8",
+    "url": "/raginreact/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0bea003488dc46a39642",
+    "url": "/raginreact/static/js/2.74e1ad95.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/raginreact/static/js/2.74e1ad95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec83b8b3e2f59dfc76e8",
+    "url": "/raginreact/static/js/main.21cd16f5.chunk.js"
+  },
+  {
+    "revision": "dc072da644be0533aef6",
+    "url": "/raginreact/static/js/runtime-main.1de832a8.js"
+  }
+]);
