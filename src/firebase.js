6f3c042db2729dabba4895704576920977292342
Ragin <mmu@ragin.org>
2020-08-14 13:56:10 +0200

    First commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBJkjyqN9ni_iRmVA5xHXeSKd9rUZRw9N0",
+    authDomain: "raginreact.firebaseapp.com",
+    databaseURL: "https://raginreact.firebaseio.com",
+    projectId: "raginreact",
+    storageBucket: "raginreact.appspot.com",
+    messagingSenderId: "319927323707",
+    appId: "1:319927323707:web:d6a02629228cab2e7da01e",
+    measurementId: "G-Q7F9FGCBPY"
+  };
