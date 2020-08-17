b6b66aea2597b93aa8dd53a2b615bb103032da5b
ragin <m.g.mueller@yandex.com>
2020-08-17 13:13:53 +0200

    added material-ui to project

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React from 'react';
 
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app"> 
+    { /* BEM class naming convention */ }
+
       <header className="App-header">
        
         <p>
-          Ragin React App
+          Ragin Google React App
         </p>
+        {/* header -> <Header /> */}
+        <Header/>
+        {/* sidebar -> <Sidebar >  */}
+        {/* recommended videos ->  */}
+        
         <a
           className="App-link"
           href="https://reactjs.org"
