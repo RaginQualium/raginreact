993d8036389cbc086420e91af92ee56fe2a7604b
ragin <m.g.mueller@yandex.com>
2020-08-17 14:56:21 +0200

    formatted header elements using flexbox

--- src/App.js
+++ src/App.js
@@ -4,31 +4,18 @@ import './App.css';
 import Header from './Header';
 
 function App() {
-  return (
-    <div className="app"> 
-    { /* BEM class naming convention */ }
+    return (
+        <div className="app">
+            {/* BEM class naming convention */}
 
-      <header className="App-header">
-       
-        <p>
-          Ragin Google React App
-        </p>
-        {/* header -> <Header /> */}
-        <Header/>
-        {/* sidebar -> <Sidebar >  */}
-        {/* recommended videos ->  */}
-        
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+            <header className="App-header">
+                {/* header -> <Header /> */}
+                <Header/> 
+                {/* sidebar -> <Sidebar >  */}
+                {/* recommended videos ->  */}
+            </header>
+        </div>
+    );
 }
 
 export default App;
--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,36 @@
 import React from 'react'
 import MenuIcon from "@material-ui/icons/Menu"
+import SearchIcon from "@material-ui/icons/Search"
+import VideoCallIcon from "@material-ui/icons/VideoCall"
+import AppsIcon from "@material-ui/icons/Apps"
+import NotificationsIcon from "@material-ui/icons/Notifications"
+import Avatar from "@material-ui/core/Avatar"
+import "./Header.css"
 
 function Header() {
     return (
         <div className="header">
-            <h1>I am a header</h1>
-            <MenuIcon />
+            <div className="header__left">
+                <MenuIcon/>
+                <img
+                    className="header__logo"
+                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/Logo_of_YouTube_%282015-2017%29.svg/502px-Logo_of_YouTube_%282015-2017%29.svg.png"
+                    alt="yt logo"
+                />
+            </div>
+            <div className="header__input">
+            <input type="text" placeholder="Search" type="text"/>
+            <SearchIcon className="header__inputButton"/>
+            </div>
+
+            <div className="header__icons">
+            <VideoCallIcon className="header__icon"/>
+            <AppsIcon className="header__icon"/>
+            <NotificationsIcon className="header__icon"/>
+            <Avatar
+                src="https://avatars2.githubusercontent.com/u/44566235?s=60&u=0e51fdf444185482e211f6913bf5a60c489a562f&v=4"
+                alt="Ragin Qualium"/>
+            </div>
         </div>
     )
 }
