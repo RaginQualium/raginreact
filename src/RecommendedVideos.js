f15b3a78f5bee7919a2d5b57c608aed7f16a0cb4
ragin <m.g.mueller@yandex.com>
2020-08-19 17:33:56 +0200

    added more placeholder videocards and added  scripts

--- src/RecommendedVideos.js
+++ src/RecommendedVideos.js
@@ -8,14 +8,28 @@ function RecommendedVideos() {
             <h2>Recommended</h2>
             <div className="recommendedVideos__videos">
                  <VideoCard
-                 title="Become a web developer in 10 minutes"
-                 view="2.5M views"
-                 timestamp="3 days ago"
-                 channelImage="https://yt3.ggpht.com/a-/AOh14GhX1kTmCNgahq2-PG6cV6fW9V75FAi1ECaoDg=s68-c-k-c0x00ffffff-no-rj-mo"
-                 channel="MGM Technology"
-                 image="https://i.ytimg.com/vi/t1aXuJkmTg8/hq720.jpg?sqp=-oaymwEZCNAFEJQDSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLDapi4poBGJzUziN5WVJRpDzVpE_g"
+                    title="Become a web developer in 10 minutes"
+                    view="2.5M views"
+                    timestamp="3 days ago"
+                    channelImage="https://yt3.ggpht.com/a-/AOh14GhX1kTmCNgahq2-PG6cV6fW9V75FAi1ECaoDg=s68-c-k-c0x00ffffff-no-rj-mo"
+                    channel="MGM Technology"
+                    image="https://i.ytimg.com/vi/t1aXuJkmTg8/hq720.jpg?sqp=-oaymwEZCNAFEJQDSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLDapi4poBGJzUziN5WVJRpDzVpE_g"
                  />
                 <VideoCard
+                    title="How to build a instagram clone"
+                    view="1.5M views"
+                    timestamp="2 days ago"
+                    channelImage="https://yt3.ggpht.com/a-/AOh14GhOUP_9re93yOMdN4RLQLHl6Mbk6vmUS1524w=s68-c-k-c0x00ffffff-no-rj-mo"
+                    channel="Ragin Technology"
+                    image="https://i.ytimg.com/vi/f7T48W0cwXM/hq720.jpg?sqp=-oaymwEZCNAFEJQDSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLDb-qyj2scW4xXIGnQX8skGWKF4dw"/>
+                <VideoCard
+                    title="The truth about mechanical keyboards"
+                    view="22.5M views"
+                    timestamp="3 days ago"
+                    channelImage="https://yt3.ggpht.com/a-/AOh14GhX1kTmCNgahq2-PG6cV6fW9V75FAi1ECaoDg=s68-c-k-c0x00ffffff-no-rj-mo"
+                    channel="Tech Lead"
+                    image="https://i.ytimg.com/vi/bQGwPcZ3U70/hq720.jpg?sqp=-oaymwEZCNAFEJQDSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLBz8sIVZkQ-V3VTgYQw4Mb9_fdqbw"/>
+                <VideoCard
                     title="Become a web developer in 10 minutes"
                     view="2.5M views"
                     timestamp="3 days ago"
@@ -28,7 +42,51 @@ function RecommendedVideos() {
                     timestamp="3 days ago"
                     channelImage="https://yt3.ggpht.com/a-/AOh14GhX1kTmCNgahq2-PG6cV6fW9V75FAi1ECaoDg=s68-c-k-c0x00ffffff-no-rj-mo"
                     channel="MGM Technology"
+                    image="https://i.ytimg.com/vi/t1aXuJkmTg8/hq720.jpg?sqp=-oaymwEZCNAFEJQDSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLDapi4poBGJzUziN5WVJRpDzVpE_g"
+                 />
+                <VideoCard
+                    title="How to build a instagram clone"
+                    view="1.5M views"
+                    timestamp="2 days ago"
+                    channelImage="https://yt3.ggpht.com/a-/AOh14GhOUP_9re93yOMdN4RLQLHl6Mbk6vmUS1524w=s68-c-k-c0x00ffffff-no-rj-mo"
+                    channel="Ragin Technology"
+                    image="https://i.ytimg.com/vi/f7T48W0cwXM/hq720.jpg?sqp=-oaymwEZCNAFEJQDSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLDb-qyj2scW4xXIGnQX8skGWKF4dw"/>
+                <VideoCard
+                    title="The truth about mechanical keyboards"
+                    view="22.5M views"
+                    timestamp="3 days ago"
+                    channelImage="https://yt3.ggpht.com/a-/AOh14GhX1kTmCNgahq2-PG6cV6fW9V75FAi1ECaoDg=s68-c-k-c0x00ffffff-no-rj-mo"
+                    channel="Tech Lead"
+                    image="https://i.ytimg.com/vi/bQGwPcZ3U70/hq720.jpg?sqp=-oaymwEZCNAFEJQDSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLBz8sIVZkQ-V3VTgYQw4Mb9_fdqbw"/>
+                <VideoCard
+                    title="Become a web developer in 10 minutes"
+                    view="2.5M views"
+                    timestamp="3 days ago"
+                    channelImage="https://yt3.ggpht.com/a-/AOh14GhX1kTmCNgahq2-PG6cV6fW9V75FAi1ECaoDg=s68-c-k-c0x00ffffff-no-rj-mo"
+                    channel="MGM Technology"
                     image="https://i.ytimg.com/vi/t1aXuJkmTg8/hq720.jpg?sqp=-oaymwEZCNAFEJQDSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLDapi4poBGJzUziN5WVJRpDzVpE_g"/>
+                <VideoCard
+                    title="Become a web developer in 10 minutes"
+                    view="2.5M views"
+                    timestamp="3 days ago"
+                    channelImage="https://yt3.ggpht.com/a-/AOh14GhX1kTmCNgahq2-PG6cV6fW9V75FAi1ECaoDg=s68-c-k-c0x00ffffff-no-rj-mo"
+                    channel="MGM Technology"
+                    image="https://i.ytimg.com/vi/t1aXuJkmTg8/hq720.jpg?sqp=-oaymwEZCNAFEJQDSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLDapi4poBGJzUziN5WVJRpDzVpE_g"
+                 />
+                <VideoCard
+                    title="How to build a instagram clone"
+                    view="1.5M views"
+                    timestamp="2 days ago"
+                    channelImage="https://yt3.ggpht.com/a-/AOh14GhOUP_9re93yOMdN4RLQLHl6Mbk6vmUS1524w=s68-c-k-c0x00ffffff-no-rj-mo"
+                    channel="Ragin Technology"
+                    image="https://i.ytimg.com/vi/f7T48W0cwXM/hq720.jpg?sqp=-oaymwEZCNAFEJQDSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLDb-qyj2scW4xXIGnQX8skGWKF4dw"/>
+                <VideoCard
+                    title="The truth about mechanical keyboards"
+                    view="734K views"
+                    timestamp="3 days ago"
+                    channelImage="https://yt3.ggpht.com/a-/AOh14GhX1kTmCNgahq2-PG6cV6fW9V75FAi1ECaoDg=s68-c-k-c0x00ffffff-no-rj-mo"
+                    channel="Tech Lead"
+                    image="https://i.ytimg.com/vi/bQGwPcZ3U70/hq720.jpg?sqp=-oaymwEZCNAFEJQDSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLBz8sIVZkQ-V3VTgYQw4Mb9_fdqbw"/>
                 <VideoCard
                     title="Become a web developer in 10 minutes"
                     view="2.5M views"
