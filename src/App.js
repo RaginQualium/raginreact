6f242735a4b0323e7a01994039f41cb99418b54f
ragin <m.g.mueller@yandex.com>
2020-08-20 09:03:17 +0200

    put in router

--- src/App.js
+++ src/App.js
@@ -5,23 +5,21 @@ import Sidebar from './Sidebar';
 import RecommendedVideos from './RecommendedVideos';
 
 import {
-    BrowserRouter as Router,
-    Switch,
-    Route
-  } from "react-router-dom";
+    BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import './App.css';
 
 function App() {
     return (
-        <div className="app">{/* BEM class naming convention */}
+        <div className="app"> {/* BEM class naming convention : lowercase app*/}
             <Router>
                 <Header/> 
                 <Switch>
-                    <Route path="/search/:searchTerm">
+                    <Route exact path="/search">
                         <h1>Search page</h1>
                     </Route>
                     <Route path="/">
                         <div className="app__page">
+                            <p>homepage</p>
                             <Sidebar />
                             <RecommendedVideos/>
                         </div>
